8b4c5690512a27557a6d06c44afd0fd845e7c6dd
LuizF6856 <luizfellipe6843@gmail.com>
2021-09-20 10:33:41 -0300

    Deleted

--- test/ExemplosDeCodigos.exs
+++
@@ -1,7 +0,0 @@
-defmodule Exemplos_codigos do
-  use ExUnit.Case
-
-  test "getname" do
-    assert Hello.getname() == "seu nome?"
-  end
-end
--- test/Exemplos_codigos_test.exs
+++
@@ -1,7 +0,0 @@
-defmodule ExemploCodigos.Test do
-  use ExUnit.Case, async: true
-
-  test "getname" do
-    assert App.Hello.getname == "Ola "
-  end
-end
