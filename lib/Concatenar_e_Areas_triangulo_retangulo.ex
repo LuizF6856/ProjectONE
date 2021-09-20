9753bc536a11493fd2169a654af2145bf92872ea
LuizF6856 <luizfellipe6843@gmail.com>
2021-09-20 10:34:34 -0300

    Comentados porque não tem teste unitario

--- lib/Concatenar_e_Areas_triangulo_retangulo.ex
+++ lib/Concatenar_e_Areas_triangulo_retangulo.ex
@@ -1,32 +1,32 @@
-defmodule Concatenar do
-  def nome(nome1) do
-    IO.puts("Olá #{nome1}, Boa tarde")
-  end
-end
+# defmodule Concatenar do
+#   def nome(nome1) do
+#     IO.puts("Olá #{nome1}, Boa tarde")
+#   end
+# end
 
-defmodule Area do
-  def triangulo(base, altura) do
-    bh = base * altura / 2
-    IO.puts("O valor da base é #{base} e a altura é #{altura}, #{bh} ")
-  end
+# defmodule Area do
+#   def triangulo(base, altura) do
+#     bh = base * altura / 2
+#     IO.puts("O valor da base é #{base} e a altura é #{altura}, #{bh} ")
+#   end
 
-  def retangulo(lado, lado1) do
-    lados = lado * lado1
-    IO.puts("A altura do retangulo #{lado} vezes a largura #{lado1}, da o total da area #{lados}")
-  end
-end
+#   def retangulo(lado, lado1) do
+#     lados = lado * lado1
+#     IO.puts("A altura do retangulo #{lado} vezes a largura #{lado1}, da o total da area #{lados}")
+#   end
+# end
 
-# PATTERN MATCHING dos codigos acima
+# # PATTERN MATCHING dos codigos acima
 
-defmodule Concatenar do
-  def nome(nome1), do: "Olá #{nome1}, Boa tarde"
-end
+# defmodule Concatenar do
+#   def nome(nome1), do: "Olá #{nome1}, Boa tarde"
+# end
 
-defmodule Area do
-  def triangulo(base, altura),
-    do: "O valor da base é #{base} e a altura é #{altura}, #{base * altura / 2}"
+# defmodule Area do
+#   def triangulo(base, altura),
+#     do: "O valor da base é #{base} e a altura é #{altura}, #{base * altura / 2}"
 
-  def retangulo(lado, lado1),
-    do:
-      "A altura do retangulo #{lado} vezes a largura #{lado1}, da o total da area #{lado * lado1}"
-end
+#   def retangulo(lado, lado1),
+#     do:
+#       "A altura do retangulo #{lado} vezes a largura #{lado1}, da o total da area #{lado * lado1}"
+# end
--- lib/Loop_com_Lista.ex
+++ lib/Loop_com_Lista.ex
@@ -1,39 +1,39 @@
-# Usando listas mais loop
+# # Usando listas mais loop
 
-defmodule List do
-  def soma([head | tail], acumulador) do
-    soma(tail, head + acumulador)
-  end
+# defmodule List do
+#   def soma([head | tail], acumulador) do
+#     soma(tail, head + acumulador)
+#   end
 
-  def soma([], acumulador) do
-    acumulador
-  end
-end
+#   def soma([], acumulador) do
+#     acumulador
+#   end
+# end
 
-# PATTERN MATCHING
+# # PATTERN MATCHING
 
-defmodule List do
-  def soma([head | tail], acumulador), do: soma(tail, head + acumulador)
-  def soma([], acumulador), do: acumulador
-end
+# defmodule List do
+#   def soma([head | tail], acumulador), do: soma(tail, head + acumulador)
+#   def soma([], acumulador), do: acumulador
+# end
 
-# Para chamar o modulo, List.soma [1,2,3,4], 0
+# # Para chamar o modulo, List.soma [1,2,3,4], 0
 
-# - TESTE DE MESA PARA MOSTRAR COMO FUNCIONA O LOOP -
+# # - TESTE DE MESA PARA MOSTRAR COMO FUNCIONA O LOOP -
 
-# [1,2,3,4], 0 / .1 #entrada
-# [2,3,4], 1 + 0 / . #operação
-# [2,3,4], 1 / . #resposta
+# # [1,2,3,4], 0 / .1 #entrada
+# # [2,3,4], 1 + 0 / . #operação
+# # [2,3,4], 1 / . #resposta
 
-# [2,3,4], 1 / .2
-# [3,4], 2 + 1 / .
-# [3,4], 3 /.
+# # [2,3,4], 1 / .2
+# # [3,4], 2 + 1 / .
+# # [3,4], 3 /.
 
-# [3,4], 3 /.3
-# [4], 3 + 3/.
-# [4], 6 /.
+# # [3,4], 3 /.3
+# # [4], 3 + 3/.
+# # [4], 6 /.
 
-# [4], 6 / .4
-# [], 4 + 6 / .
-# Chegando aqui o computador vai descer para a outra função 
-# [], 10 /.
+# # [4], 6 / .4
+# # [], 4 + 6 / .
+# # Chegando aqui o computador vai descer para a outra função 
+# # [], 10 /.
