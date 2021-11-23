7e460abcd65200bbfd578624fc75cffc498a2917
LuizF6856 <luizfellipe6843@gmail.com>
2021-11-23 19:07:23 -0300

    remove unit test getname

--- test/Exemplos_codigos_test.exs
+++ test/Exemplos_codigos_test.exs
@@ -1,12 +1,7 @@
 defmodule ExemploCodigos.Test do
   use ExUnit.Case
 
-  @tag id_fuction: 1
-  test "getname" do
-    assert App.Hello.getname == "Ola "
-  end
-
   test "soma e multiplicaçãoo com numeros aleatorios" do
-    assert App.Testando_if.chamar("somar", 1, 1) == 2
+    assert SomarEmultiplicar.chamar("somar", 1, 1) == 2
   end
 end
