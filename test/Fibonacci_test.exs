b5bd2598ac2548d504b3fc4bae5b13962db4a171
LuizF6856 <luizfellipe6843@gmail.com>
2021-11-23 18:31:52 -0300

    unit test to module Fibonacci

---
+++ test/Fibonacci_test.exs
@@ -0,0 +1,15 @@
+defmodule Fibonacci.Test do
+    use ExUnit.Case
+
+    test "a" do
+        assert Fibonacci.fib(10) == 55
+    end
+
+    test "b" do
+        assert Fibonacci.fib("a") == false
+    end
+
+    test "c" do
+        assert Fibonacci.fib(true) == false
+    end
+end
