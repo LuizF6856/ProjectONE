79d510535c2401ebbaacd322c278b5d590c15723
LuizF6856 <luizfellipe6843@gmail.com>
2021-11-23 18:39:27 -0300

    improve Verificador2

--- lib/Verificador_numeroEM_Lista.ex
+++ lib/Verificador_numeroEM_Lista.ex
@@ -1,34 +1,31 @@
-# defmodule Verificador do
-#   def recursao(lista, p) do
-#     for x <- lista do
-#       if(x == p) do
-#         IO.puts("O número #{p} pertence a lista")
-#       else
-#         IO.puts("O número #{p} não pertence a lista")
-#       end
-#     end
-#   end
-# end
+defmodule Verificador1 do
+  def recursao?(lista, p) do
+    for x <- lista do
+      if(x == p) do
+        IO.puts("O numero #{p} pertence a lista")
+      else
+        IO.puts("O numero #{p} no pertence a lista")
+      end
+    end
+  end
+end
 
-# defmodule Verificador do
-#   def recursao(lista, p),
-#     do:
-#       for(
-#         x <- lista,
-#         do:
-#           if(x == p,
-#             do: " O número #{p} pertence a lista",
-#             else: " O número #{p} não pertence a lista"
-#           )
-#       )
-# end
+defmodule Verificador2 do
+  def recursao?(lista, p) do
+    if(p in lista) do
+        "O numero #{p} pertence a lista"
+        else
+        "O numero #{p} nao pertence a lista"
+    end
+  end
+end
 
-# defmodule A do
-#   defp contains?([number | list], number), do: IO.puts("#{number} is in the list")
-#   defp contains?([number | list], n), do: contains?(list, n)
-#   defp contains?([], number), do: IO.puts("#{number} is NOT in the list")
+defmodule Verificador3 do
+  defp contains?([number | list], number), do: IO.puts("#{number} is in the list")
+  defp contains?([number | list], n), do: contains?(list, n)
+  defp contains?([], number), do: IO.puts("#{number} is NOT in the list")
 
-#   def is_in_the_list(list, n) do
-#     contains?(list, n)
-#   end
-# end
+  def is_in_the_list(list, n) do
+    contains?(list, n)
+  end
+end
