413b1bab704157312d392cc31384bb9ea41d4bc5
LuizF6856 <luizfellipe6843@gmail.com>
2021-11-24 12:40:50 -0300

    inspect
    Description: inspect to show the list changing as the computer enters it

--- lib/Verificador_numeroEM_Lista.ex
+++ lib/Verificador_numeroEM_Lista.ex
@@ -21,9 +21,23 @@ defmodule Verificador2 do
 end
 
 defmodule Verificador3 do
-  defp contains?([number | list], number), do: IO.puts("#{number} is in the list")
-  defp contains?([number | list], n), do: contains?(list, n)
-  defp contains?([], number), do: IO.puts("#{number} is NOT in the list")
+  defp contains?([number | list], number) do
+    IO.inspect(number)
+
+     IO.puts("#{number} is in the list")
+  end
+
+  defp contains?([number | list], n) do
+     IO.inspect([number | list]) 
+     
+     contains?(list, n)
+  end
+  
+  defp contains?([], number) do 
+    IO.inspect(number)
+    
+    IO.puts("#{number} is NOT in the list")
+  end
 
   def is_in_the_list(list, n) do
     contains?(list, n)
