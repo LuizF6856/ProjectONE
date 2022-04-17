dd29fd2cf63e77ab3dd05a9a99dd9abe030aee0b
LuizF6856 <luizfellipe6843@gmail.com>
2022-04-16 22:59:58 -0300

    code more simple

--- lib/Calculadora.ex
+++ lib/Calculadora.ex
@@ -1,29 +1,13 @@
-defmodule CalculadoraV2 do
+defmodule Calculadora do
+  def somar(numero, numero_1) when is_number(numero) and is_number(numero_1),
+    do: "#{numero} + #{numero_1} = #{numero + numero_1}"
 
-    defp somar(n, n1), do: n + n1
+  def subtrair(numero, numero_1) when is_number(numero) and is_number(numero_1),
+    do: "#{numero} - #{numero_1} = #{numero - numero_1}"
 
-    defp subtrair(n, n1), do: n - n1
+  def multiplicar(numero, numero_1) when is_number(numero) and is_number(numero_1),
+    do: "#{numero} x #{numero_1} = #{numero * numero_1}"
 
-    defp multiplicar(n, n1), do: n * n1
-
-    defp dividir(n, n1), do: n / n1
-
-    def operador(opr, n, n1) do
-        cond do
-            opr == "somar"
-                -> somar(n, n1)
-
-            opr == "subtrair"
-                -> subtrair(n, n1)
-
-            opr == "multiplicar"
-                -> multiplicar(n, n1)
-
-            opr == "dividir"
-                -> dividir(n, n1)
-
-            true -> 
-                "Apenas somar, subtrair, multiplicar e dividir"
-        end
-    end
+  def dividir(numero, numero_1) when is_number(numero) and is_number(numero_1),
+    do: "#{numero} / #{numero_1} = #{numero / numero_1}"
 end
