5bbd6e190b112f757b029fbb88976d653a3fe1b1
LuizF6856 <luizfellipe6843@gmail.com>
2022-04-17 03:01:36 -0300

    difference calculator

---
+++ lib/CalculadoraV2.ex
@@ -0,0 +1,36 @@
+defmodule App.CalculatorV2 do
+  def your_calculation do
+    IO.gets("what would you like to calculate?")
+    |> String.trim()
+    |> decode()
+  end
+
+  defp decode(result) do
+    calculation =
+      Regex.split(~r{}, result) -- ["", ""]
+
+    [number,operator,number_1] = calculation
+
+   num = String.to_integer(number)
+   num1 = String.to_integer(number_1)
+
+    case operator do
+       "+" ->
+          sum(num, num1)
+       "-" ->
+          subtract(num, num1)
+       "x" ->
+          multiply(num, num1)
+       "/" ->
+          divide(num, num1)
+       _ ->
+        {:error, "only: sum/2, subtract/2, multiply/2, divide/2"}
+
+    end
+  end
+
+  defp sum(n,n1), do: {:ok, "#{n} + #{n1} = #{n + n1}"}
+  defp subtract(n,n1), do: {:ok, "#{n} + #{n1} = #{n - n1}"}
+  defp multiply(n,n1), do: {:ok,"#{n} + #{n1} = #{n * n1}"}
+  defp divide(n,n1), do: {:ok, "#{n} + #{n1} = #{n / n1}"}
+end
