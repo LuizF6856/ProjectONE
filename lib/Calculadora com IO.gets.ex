3873fca53d0c2434e13046b4090a6b12c51e8f4a
LuizF6856 <luizfellipe6843@gmail.com>
2022-05-10 15:23:40 -0300

    mix format

--- lib/Calculadora com IO.gets.ex
+++ lib/Calculadora com IO.gets.ex
@@ -6,31 +6,33 @@ defmodule App.CalculatorV2 do
   end
 
   defp decode(result) do
-    calculation =
-      Regex.split(~r{}, result) -- ["", ""]
+    calculation = Regex.split(~r{}, result) -- ["", ""]
 
-    [number,operator,number_1] = calculation
+    [number, operator, number_1] = calculation
 
-   num = String.to_integer(number)
-   num1 = String.to_integer(number_1)
+    num = String.to_integer(number)
+    num1 = String.to_integer(number_1)
 
     case operator do
-       "+" ->
-          sum(num, num1)
-       "-" ->
-          subtract(num, num1)
-       "x" ->
-          multiply(num, num1)
-       "/" ->
-          divide(num, num1)
-       _ ->
-        {:error, "only: sum, subtract, multiply and divide"}
+      "+" ->
+        sum(num, num1)
 
+      "-" ->
+        subtract(num, num1)
+
+      "x" ->
+        multiply(num, num1)
+
+      "/" ->
+        divide(num, num1)
+
+      _ ->
+        {:error, "only: sum, subtract, multiply and divide"}
     end
   end
 
-  defp sum(n,n1), do: {:ok, "#{n} + #{n1} = #{n + n1}"}
-  defp subtract(n,n1), do: {:ok, "#{n} - #{n1} = #{n - n1}"}
-  defp multiply(n,n1), do: {:ok,"#{n} x #{n1} = #{n * n1}"}
-  defp divide(n,n1), do: {:ok, "#{n} / #{n1} = #{n / n1}"}
+  defp sum(n, n1), do: {:ok, "#{n} + #{n1} = #{n + n1}"}
+  defp subtract(n, n1), do: {:ok, "#{n} - #{n1} = #{n - n1}"}
+  defp multiply(n, n1), do: {:ok, "#{n} x #{n1} = #{n * n1}"}
+  defp divide(n, n1), do: {:ok, "#{n} / #{n1} = #{n / n1}"}
 end
